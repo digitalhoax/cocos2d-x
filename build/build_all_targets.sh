bf4366c2327f9a3db0618eb3634c2e3bac1274b4
Lennert Raesch <lennert@raesch.org>
2015-12-01 14:38:50 +0100

    added xcpretty to xcodebuild script

--- build/build_all_targets.sh
+++ build/build_all_targets.sh
@@ -1,10 +1,10 @@
-xcodebuild -project cocos2d_libs.xcodeproj -scheme "libcocos2d tvOS" -configuration Debug -derivedDataPath .
-xcodebuild -project cocos2d_libs.xcodeproj -scheme "libcocos2d tvOS" -configuration Release -derivedDataPath .
+xcodebuild -project cocos2d_libs.xcodeproj -scheme "libcocos2d tvOS" -configuration Debug -derivedDataPath . | xcpretty
+xcodebuild -project cocos2d_libs.xcodeproj -scheme "libcocos2d tvOS" -configuration Release -derivedDataPath . | xcpretty
 
-xcodebuild -project cocos2d_libs.xcodeproj -scheme "libcocos2d Mac" -configuration Debug -derivedDataPath .
-xcodebuild -project cocos2d_libs.xcodeproj -scheme "libcocos2d Mac" -configuration Release -derivedDataPath .
+xcodebuild -project cocos2d_libs.xcodeproj -scheme "libcocos2d Mac" -configuration Debug -derivedDataPath . | xcpretty
+xcodebuild -project cocos2d_libs.xcodeproj -scheme "libcocos2d Mac" -configuration Release -derivedDataPath . | xcpretty
 
-xcodebuild -project cocos2d_libs.xcodeproj -scheme "libcocos2d iOS" -configuration Debug -derivedDataPath .
-xcodebuild -project cocos2d_libs.xcodeproj -scheme "libcocos2d iOS" -configuration Release -derivedDataPath .
+xcodebuild -project cocos2d_libs.xcodeproj -scheme "libcocos2d iOS" -configuration Debug -derivedDataPath . | xcpretty
+xcodebuild -project cocos2d_libs.xcodeproj -scheme "libcocos2d iOS" -configuration Release -derivedDataPath . | xcpretty
 
 ./move_libs.sh
